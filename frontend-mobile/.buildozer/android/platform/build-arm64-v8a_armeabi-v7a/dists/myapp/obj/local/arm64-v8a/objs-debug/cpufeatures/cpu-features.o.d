c72604bcf8a24e50e5d5bddc8b1f8a87077efea0
PRRoid26 <easwaranshetty.com>
2026-05-07 14:57:23 +0530

    location memory

--- frontend-mobile/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/dists/myapp/obj/local/arm64-v8a/objs-debug/cpufeatures/cpu-features.o.d
+++
@@ -1,5 +0,0 @@
-/home/prroid/inventory-system/frontend-mobile/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/arm64-v8a/objs-debug/cpufeatures/cpu-features.o: \
-  /home/prroid/.buildozer/android/platform/android-ndk-r25b/sources/android/cpufeatures/cpu-features.c \
-  /home/prroid/.buildozer/android/platform/android-ndk-r25b/sources/android/cpufeatures/cpu-features.h
-
-/home/prroid/.buildozer/android/platform/android-ndk-r25b/sources/android/cpufeatures/cpu-features.h:
